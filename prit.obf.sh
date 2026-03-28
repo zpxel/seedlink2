c8f6d7e9d588f40e216e972d95281420d7615514
zpxel <zpixeljy@gmail.com>
2026-03-28 22:29:50 +0800

    Delete prit.obf.sh

--- prit.obf.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-# Read encoded payload
-ENCODED_SCRIPT="$(cat encoded.txt)"
-
-# Decode + unzip + execute
-echo "$ENCODED_SCRIPT" | base64 -d | gzip -d | bash
